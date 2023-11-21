9765aa9dae3dcb19586baf26460d1ea234087a28
Miguel Dionísio <miguelmdionisio@gmail.com>
2023-11-21 23:51:14 +0000

    wtv

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,8 +3,8 @@
 @section('content') 
     <div class="profile-container">
         <h1 class="profile-heading">Profile</h1>
-        <h2 class="profile-name">Name: {{ $user->firstname }} {{ $user->lastname }}</h2>
-        <h2 class="profile-username">Username: {{ $user->username }}</h2>
+        <h2 class="profile-name">{{ $user->firstname }} {{ $user->lastname }}</h2>
+        <h2 class="profile-username">{{ $user->username }}</h2>
         <h3 class="profile-about">About me: {{ $user->aboutme }}</h3>
 
         <h3 class="profile-posts-heading">Posts:</h3>
