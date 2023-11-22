fb90f402c4370f138e7fe8d7fd639aa4d21347b2
Miguel Dionísio <miguelmdionisio@gmail.com>
2023-11-22 15:08:51 +0000

    done

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,20 @@
-
 @extends('layouts.app')
 
-
 @section('content')
+    <section id="post-form">
+        @auth
+            <form action="{{ route('posts.create') }}" method="POST">
+                @csrf
+                <br>
+                <label for="content">Content:</label>
+                <textarea name="content" required></textarea>
+                <br>
+                <!-- Add other form fields as needed -->
+                <button type="submit">Create Post</button>
+            </form>
+        @endauth
+    </section>
+
     <section id="posts">
         @each('partials.posts', $posts, 'post')
     </section>
--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,7 @@ Route::controller(RegisterController::class)->group(function () {
 
 Route::controller(PostController::class)->group(function () {
     Route::get('/home', 'homeFeed')->name('posts');
+    Route::post('/posts', 'create')->name('posts.create');
 });
 
 Route::controller(UserController::class)->group(function (){
