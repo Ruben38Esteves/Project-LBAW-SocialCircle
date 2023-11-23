ab3caf593389e11e2d9cdc5fba9185247a4b50b7
Ruben Esteves <92651281+Ruben38Esteves@users.noreply.github.com>
2023-11-23 13:17:49 +0000

    now with delete button

--- resources/views/partials/posts.blade.php
+++ resources/views/partials/posts.blade.php
@@ -10,13 +10,15 @@
         </div>
     </a>
     <div class="post-content">
-        {{ $post->content }}
+        <div id="post_content" style="display: block">
+            {{ $post->content }}
+        </div>
         <div id="edit_post" style="display: none">
             <form action="{{ route('posts.create') }}" method="POST">
                 @csrf
                 <br>
                 <label for="content">Content:</label>
-                <textarea name="content" required></textarea>
+                <textarea name="content" placeholder="{{ $post->content }}" required></textarea>
                 <br>
                 <!-- Add other form fields as needed -->
                 <button type="submit">Edit</button>
@@ -25,18 +27,25 @@
         </div>
     </div>
     <?php if(Auth::check() && Auth::user()->id == $post->owner->id): ?>
-        <button id="edit_button" onclick="show_post_changer()" style="display: block">
-            Edit
-        </button>
+        <div id="post_buttons" style="display: inline-block">
+            <button id="edit_button" onclick="show_post_changer()" >
+                Edit
+            </button>
+            <button id="delete_button">
+                Delete
+            </button>
+        </div>
     <?php endif; ?>
 </div>
 <script>
     function show_post_changer() {
         document.getElementById("edit_post").style.display = "block";
-        document.getElementById("edit_button").style.display = "none";
+        document.getElementById("post_buttons").style.display = "none";
+        document.getElementById("post_content").style.display = "none";
     }
     function hide_post_changer() {
         document.getElementById("edit_post").style.display = "none";
-        document.getElementById("edit_button").style.display = "block";
+        document.getElementById("post_buttons").style.display = "inline-block";
+        document.getElementById("post_content").style.display = "block";
     }
 </script>
